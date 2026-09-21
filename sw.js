85c7e684503f2427ed6ff41a3afd4f7d3cfba3de
kukaje2nd <kukaje2nd@gmail.com>
2026-09-21 17:29:05 -0400

    Force cache refresh for recovery build

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mrcc-v2.3.0';
+const CACHE_NAME = 'mrcc-v2.2.2-recovery';
 const CORE_ASSETS = ['/', '/index.html', '/manifest.webmanifest', '/icon.svg'];
 
 self.addEventListener('install', event => {
