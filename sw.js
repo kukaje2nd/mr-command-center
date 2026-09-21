13c1568302de0786d2bb2f794447fdcf6f7229a1
kukaje2nd <kukaje2nd@gmail.com>
2026-09-21 16:08:30 -0400

    Refresh offline cache for v1.9.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mrcc-v1.9.0';
+const CACHE_NAME = 'mrcc-v1.9.1';
 const CORE_ASSETS = ['/', '/index.html', '/manifest.webmanifest', '/icon.svg'];
 
 self.addEventListener('install', event => {
